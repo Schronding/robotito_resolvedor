2e6174210a12326be76b107c4367cf07fce22487
schronding <schronding@gmail.com>
2025-05-30 09:04:02 -0600

    version inestable con mirarHorizonte

--- version_arduino.ino
+++ version_arduino.ino
@@ -52,26 +52,19 @@ void setup(){
   pinMode(IN3, OUTPUT); 
   pinMode(IN4, OUTPUT); 
 
-  Serial.println("Probando motores...");
-  moverAtras(150); 
-  delay(1000);
-  detenerMotores(); 
-  delay(1000);
+  //Serial.println("Probando motores...");
+  //moverAtras(120); delay(1000);
+  //detenerMotores(); delay(500);
 
-  moverAdelante(150); 
-  delay(1000);
-  detenerMotores(); 
-  delay(1000);
+  //moverAdelante(120); delay(1000);
+  //detenerMotores(); delay(500);
 
-  girarIzquierda(120); 
-  delay(1000); // Giro corto
-  detenerMotores(); 
-  delay(1000);
+  //girarIzquierda(120); delay(500); // Giro corto
+  //detenerMotores(); delay(500);
 
-  girarDerecha(120); 
-  delay(100); // Giro corto
-  detenerMotores();
-  Serial.println("Prueba de motores finalizada.");
+  //girarDerecha(120); delay(500); // Giro corto
+  //detenerMotores();
+  //Serial.println("Prueba de motores finalizada.");
 }
 
 void loop(){
@@ -98,6 +91,7 @@ void loop(){
   Serial.print(" cm | ");
   Serial.print(distanceIN);
   Serial.println(" in");
+  delay(500);
 
   if (distanceCM <= 5 && distanceCM > 0){
     //myServo.write(0); // This should turn the Servo head to the left to check
@@ -117,6 +111,7 @@ void loop(){
   //int servoPos = map(distanceCM, 2, 30, 0, 180);
   // rotate servo according to distance
   //myServo.write(servoPos);
+  girarIzquierda(120); 
 }
 
 void moverAtras(int velocidad){
@@ -177,15 +172,5 @@ void detenerMotores() {
   digitalWrite(IN4, LOW);
   analogWrite(ENB, 0);
   Serial.println("Motores Detenidos");
-}
-
-void mirarHorizonte(){
-  moverAdelante()
-}
-void girarCabeza(int giro){
-  if (distanceCM < 5 && distanceCM > 0){
-      myServo.write(giro)
-
-  }
 }
 
